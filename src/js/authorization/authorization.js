74a0462969308457b93eca780d8eb04f3990a1e1
Tetiana Savinykh <117289657+Galebonchik@users.noreply.github.com>
2023-05-08 18:25:28 +0200

    add favicon and button-to-top

--- src/js/authorization/authorization.js
+++ src/js/authorization/authorization.js
@@ -1,3 +1,4 @@
+ 
 const ESC_KEY_CODE = 'Escape';
 
 
@@ -86,3 +87,28 @@ signUpLink.addEventListener('click', event => {
   signUpLink.disabled = true;
   signInLink.disabled = false;
 });
+
+
+
+const buttonToTop  = document.querySelector('.button-to-top');
+
+window.addEventListener('scroll', onScroll);
+buttonToTop.addEventListener('click', onButtonToTop);
+
+function onScroll() {
+  const scrolled = window.pageYOffset;
+  const size  = document.documentElement.clientHeight;
+
+  if (scrolled > size) {
+    buttonToTop .classList.add('button-to-top--visible');
+  }
+  if (scrolled < size) {
+    buttonToTop .classList.remove('button-to-top--visible');
+  }
+}
+
+function onButtonToTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
